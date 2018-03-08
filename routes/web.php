7e13771e4c81a818260cb0dc8f7ac71ad9989058
Shyam Sasi T <shyam.sasi@microobjects.net>
2018-03-08 16:46:44 +0530

    first commit

---
+++ routes/web.php
@@ -0,0 +1,46 @@
+<?php
+use Illuminate\Http\Request;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function () {
+    $links = \App\Link::all();
+return view('welcome')->with('links', $links);
+});
+Route::get('/submit', function () {
+    return view('submit');
+});
+
+
+Route::post('/submit', function (Request $request) {
+    $data = $request->validate([
+        'title' => 'required|max:255',
+        'url' => 'required|url|max:255',
+        'description' => 'required|max:255',
+    ]);
+
+    $link = new \App\Link;
+$link->title = $data['title'];
+$link->url = $data['url'];
+$link->description = $data['description'];
+
+// Save the model
+$link->save();
+
+    return redirect('/');
+});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
